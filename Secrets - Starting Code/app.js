31c225e28083184a962886ee854433e00e8d2d7d
Savan <sawanpatel2508@gmail.com>
2023-09-05 11:08:00 -0400

    Encryption level 6 oauth authentication with google

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -7,7 +7,8 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -28,21 +29,54 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true, use
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id).then(function(user) {
+        done(null, user);
+    });
+});
+
+// Google OAuth 2.0
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+},
+function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+    });
+}));
 
 app.get("/", function(req, res){
     res.render("home");
 });
+
+// Google OAuth 2.0
+app.get("/auth/google",
+    passport.authenticate("google", { scope: ["profile"] }
+    ));
+
+app.get("/auth/google/secrets",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    function(req, res) {
+        // Successful authentication, redirect to secrets.
+        res.redirect("/secrets");
+    });
 
 app.get("/login", function(req, res){
     res.render("login");
@@ -74,8 +108,12 @@ app.post("/register", function(req, res){
 });
 
 app.get("/logout", function(req, res){
-    req.logout();
-    res.redirect("/");
+    req.logout(function (err) {
+        if(err){
+            console.log(err);
+        }
+        res.redirect("/");
+    });
 });
 
 app.post("/login", function(req, res){
